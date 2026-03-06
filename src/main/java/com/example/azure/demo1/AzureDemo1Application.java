1a5dc6ed3367f20534f8f621f4be49b7a1110711
anusha-ivo <anusha525@gmail.com>
2026-03-06 13:54:35 +0530

    branch creation

---
+++ src/main/java/com/example/azure/demo1/AzureDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.azure.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemo1Application.class, args);
+	}
+
+}
